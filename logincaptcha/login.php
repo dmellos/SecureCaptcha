8065a4386f57dd75e24933d690ddf2a8f207a0d0
SELWYN DMELLO <dmelloselwyn@gmail.com>
2017-01-15 22:37:08 -0500

    Update login.php

--- logincaptcha/login.php
+++ logincaptcha/login.php
@@ -4,7 +4,6 @@
 <title> login system </title>
 <link rel ="stylesheet" type="text/css" href="style.css">
 </head>
-
 <body>
 <div id="frm">
 <form action ="process.php" method ="POST" onsubmit="return Validate()" name="valform">
